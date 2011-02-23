6cd501a5bb08162f4dabeafe6f7a8bf0ae0d8eca
Sylvain Viollon <thefunny@gmail.com>
2011-02-23 14:35:46 +0100

    Refactor obviel to separate registry from the default module.

--- src/silva/ui/static/js/thirdparty/obviel.js
+++ src/silva/ui/static/js/thirdparty/obviel.js
@@ -1,24 +1,28 @@
 var obviel = {};
 
 (function($, module) {
+    // Exceptions
+    module.DuplicateInterfaces = function(name) {
+        this.message = 'duplicate registration of interface ' + name;
+    };
+    module.UnknownIface = function(name) {
+        this.message = 'iface ' + name + ' not found';
+    };
+    module.InvalidOptions = function(options) {
+        this.message = 'Missing required options';
+    };
+
     // 'interface' implementation - interfaces are just strings that are
     // registered as a tree
     module._ifaces = {
         'base': []
     };
 
+    /**
+     * Register an interface
+     * @param name: interface name (string)
+     */
     module.iface = function(name) {
-        /* register an interface
-
-           register an iface with name 'name' (string), if other arguments
-           are passed to this function, consider the rest base ifaces
-           (supers), in order of importance
-
-           interfaces are just strings, used as markers
-
-           note that if an iface is registered, it automatically inherits
-           from 'base'
-        */
         if (module._ifaces[name]) {
             throw((new module.DuplicateInterfaces(name)));
         };
@@ -43,33 +47,10 @@ var obviel = {};
         module._ifaces[name] = bases;
     };
 
-    module.implements = function(obj, base) {
-        /* returns true of obj implements base, false otherwise
-        */
-        var ifaces = module.ifaces(obj);
-        for (var i=0; i < ifaces.length; i++) {
-            if (ifaces[i] == base) {
-                return true;
-            };
-        };
-        return false;
-    };
-
-    module.extendsIface = function(name, base) {
-        /* register a new base for interface name
-        */
-        var basebases = module._ifaces[base];
-        if (basebases === undefined) {
-            throw((new module.UnknownIface(base)));
-        };
-        for (var i=0; i < basebases.length; i++) {
-            if (basebases[i] == name) {
-                throw((new module.RecursionError(name, basebases[i])));
-            };
-        };
-        module._ifaces[name].push(base);
-    };
-
+    /**
+     * Return a list of interface implemented by the object
+     * @param obj: object to interspect.
+     */
     module.ifaces = function(obj) {
         /* return the interfaces of an obj, breadth first
         */
@@ -107,26 +88,12 @@ var obviel = {};
         return ret;
     };
 
-    // Registered views
-    module._views = {}; // name to dict {name: iface: view}
-
-    // Exceptions
-    module.DuplicateInterfaces = function(name) {
-        this.message = 'duplicate registration of interface ' + name;
-    };
-
-    module.RecursionError = function(name, base) {
-        this.message = 'recursion on base ' + base + ' for name ' + name;
-    };
-
-    module.UnknownIface = function(name) {
-        this.message = 'iface ' + name + ' not found';
-    };
-    module.InvalidOptions = function(options) {
-        this.message = 'Missing required options';
-    };
-
-    // View class
+    /**
+     * View
+     * @param definition: definition of the view (object containing settings and methods)
+     * @param element: DOM element on which the view will be rendered
+     * @param data: data object used by the view to be rendered.
+     */
     module.View = function(definition, element, data) {
         $.extend(this, definition);
         this.content = element;
@@ -137,18 +104,20 @@ var obviel = {};
         };
     };
 
-    module.View.prototype.doRender = function(name, callback) {
+    // render the view
+    module.View.prototype._render = function(callback) {
         this.content.trigger('cleanup.obviel');
 
         this._get_template(function(html) {
-            this._process_content(html, name, callback);
+            this._render_template(html, callback);
             if (this.cleanup) {
                 this.content.one('cleanup.obviel', this.cleanup.scope(this));
             };
         }.scope(this));
     };
 
-    module.View.prototype._process_content = function(template, name, callback) {
+    // render the view using the given template
+    module.View.prototype._render_template = function(template, callback) {
         if (this.iframe) {
             var iframe = document.createElement('iframe');
             iframe.src = '';
@@ -197,7 +166,10 @@ var obviel = {};
         };
     };
 
-    module._template_cache = {}; // mapping url or html to template instance
+    // template cache: mapping url or html to template instance or html
+    module._template_cache = {};
+
+    // retrieve a template for a view
     module.View.prototype._get_template = function(callback) {
         if (this.data.html || (this.html && !this.data.html_url) ||
             this.data.jsont || (this.jsont && !this.data.jsont_url)) {
@@ -253,21 +225,20 @@ var obviel = {};
         };
     };
 
-    /* view registration
+    /**
+     * Registery to used register views and render them.
+     */
+    module.Registry = function() {
+        // Registered views
+        this._views = {}; // name to dict {name: iface: view}
+    };
 
-       use $.view(viewobj) to register a view instance
-
-       view objects should support a 'render' method that will be called when
-       the view is rendered - with the element, a context obj and the
-       registered name as arguments - and can optionally have an attr 'name'
-       that gives the view a name (defaulting to 'default') and an 'iface' attr
-       that defines what kind of context object ifaces are accepted (these are
-       merely string markers, no real iface objects)
-    */
-    module.view = function(definition) {
-        /* register a view
-
-           the view is registered by viewobj.name, defaulting to 'default',
+    /**
+     * Register a new view.
+     * @param definition: associated definition for the view.
+     */
+    module.Registry.prototype.register = function(definition) {
+        /* the view is registered by viewobj.name, defaulting to 'default',
            and viewobj.iface, defaulting to 'object' (which is implemented
            by all JS objects), and order to 0 (used for multiple rendering).
         */
@@ -283,18 +254,18 @@ var obviel = {};
         };
 
         // Registration
-        if (!module._views[definition.name]) {
-            module._views[definition.name] = {};
+        if (!this._views[definition.name]) {
+            this._views[definition.name] = {};
         };
-        if (!module._views[definition.name][definition.iface]) {
-            module._views[definition.name][definition.iface] = [];
+        if (!this._views[definition.name][definition.iface]) {
+            this._views[definition.name][definition.iface] = [];
         };
-        module._views[definition.name][definition.iface].unshift(definition);
+        this._views[definition.name][definition.iface].unshift(definition);
     };
 
-    module._render_data = function(element, data, args) {
+    module.Registry.prototype._render_data = function(element, data, args) {
         var ifaces = module.ifaces(data);
-        var views = module._views[args.name];
+        var views = this._views[args.name];
         var to_render = null;
 
         for (var i=0; i < ifaces.length; i++) {
@@ -305,20 +276,20 @@ var obviel = {};
                 if (args.extra) {
                     $.extend(view_definition, args.extra);
                 };
-                to_render = new obviel.View(view_definition, element, data);
+                to_render = new module.View(view_definition, element, data);
                 break;
             };
         };
         if (to_render != null) {
-            to_render.doRender(args.name, args.callback);
+            to_render._render(args.callback);
         } else if (window.console && console.log) {
             console.log('failed view lookup for args', args, 'and', data);
         };
     };
 
-    module._render_every_data = function(element, data, args) {
+    module.Registry.prototype._render_every_data = function(element, data, args) {
         var ifaces = module.ifaces(data);
-        var views = module._views[args.name];
+        var views = this._views[args.name];
         var to_render = [];
 
         if (views == undefined) {
@@ -333,7 +304,7 @@ var obviel = {};
                     if (args.extra) {
                         $.extend(view_definition, args.extra);
                     };
-                    var view = new obviel.View(view_definition, element, data);
+                    var view = new module.View(view_definition, element, data);
                     if (view.available != undefined)
                         if (!view.available()) {
                             return;
@@ -346,50 +317,61 @@ var obviel = {};
             return v1.order - v2.order;
         });
         $.each(to_render, function (i, view) {
-            view.doRender(args.name, args.callback);
+            view._render(args.callback);
         });
     };
 
-    module._render_url = function(element, url, args) {
+    module.Registry.prototype._render_url = function(element, url, args) {
         $.ajax({
             type: 'GET',
             url: url,
             success: function(obj) {
-                module._render_data(element, obj, args);
-            },
+                this._render_data(element, obj, args);
+            }.scope(this),
             dataType: 'json'
         });
     };
 
-    $.fn.render = function(args) {
-        /* render a view
+    /**
+     * Render a View.
 
-           arguments supported are:
+       arguments are provided as an object:
 
-             * data - the object to render using the view
-             * every - alternative object
-             * url - alternative url
-             * name - the name of the view to render, optional, defaults to
-               'default'
-             * callback - optional, a function that is called after the
-               rendering has completed, with args 'element' (the JQuery-wrapped
-               element that the view is executed on), 'view' (the view
-               instance) and 'context' (a reference to 'obj')
-        */
-        var element = $(this);
+       * data - the object to render using the view
+       * every - alternative object
+       * url - alternative url
+       * name - the name of the view to render, optional, defaults to
+        'default'
+       * callback - optional, a function that is called after the
+         rendering has completed, with args 'element' (the JQuery-wrapped
+         element that the view is executed on), 'view' (the view
+         instance) and 'context' (a reference to 'obj')
+    */
+    module.Registry.prototype.render = function(element, args) {
         if (!args.name) {
             args.name = 'default';
         };
 
         if (args.data) {
-            module._render_data(element, args.data, args);
+            this._render_data(element, args.data, args);
         } else if (args.every) {
-            module._render_every_data(element, args.every, args);
+            this._render_every_data(element, args.every, args);
         } else if(args.url) {
-            module._render_url(element, args.url, args);
+            this._render_url(element, args.url, args);
         } else {
             throw((new module.InvalidOptions(args)));
         };
+    };
+
+    // Default registry and access to it.
+    module._default_registry = new module.Registry();
+
+    module.view = function(definition) {
+        module._default_registry.register(definition);
+    };
+
+    $.fn.render = function(args) {
+        module._default_registry.render($(this), args);
     };
 
 })(jQuery, obviel);
