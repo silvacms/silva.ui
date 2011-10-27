105330085557ced8a4758b4311cc51b3dbd19cdd
Sylvain Viollon <thefunny@gmail.com>
2011-10-27 22:31:12 +0200

    Remove useless stuff.

--- src/silva/ui/static/js/form.js
+++ src/silva/ui/static/js/form.js
@@ -18,10 +18,6 @@
                 });
             };
 
-            var max = function (n1, n2) {
-                return n1 > n2 ? n1 : n2;
-            };
-
             /**
              * Scroll the field into view if it is not.
              */
